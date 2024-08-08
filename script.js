07a13df34940ca521652a8afeca6586b219b7b3b
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-08-08 12:00:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,13 @@ var mapCenter = [56.563729541410666, -3.584791427303605];
 
 function initMap() {
 
-  // This creates the Leaflet map with a generic start point, because code at bottom automatically fits bounds to all markers
+  // Create the Leaflet map
   var map = L.map('map', {
     center: mapCenter,
     zoom: 15,
     scrollWheelZoom: false,
     zoomControl: false
   });
-
-  // This displays a base layer map (other options available)
-  // var lightAll = new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-  // }).addTo(map);
 
   var ohmLayer = new L.MapboxGL({
       attribution: "OpenHistoricalMap",
@@ -62,23 +57,18 @@ function initMap() {
 
   TIMESLIDER = new L.Control.OHMTimeSlider(tsoptions).addTo(map);
 
+  map.attributionControl.setPrefix('');
 
-  // This customizes link to view source code; add your own GitHub repository
-  map.attributionControl
-  .setPrefix('');
-
-  // This loads the GeoJSON map data file from a local folder
+  // Load the GeoJSON data
   $.getJSON('map.geojson', function(data) {
-
-    var data = data.features.sort(function(a, b) {
-      return a.properties['year'] - b.properties['year'];
+    // Sort the features by the 'year' property (convert year to Date object for accurate sorting)
+    data.features.sort(function(a, b) {
+      return new Date(a.properties.year) - new Date(b.properties.year);
     });
 
     var geojson = L.geoJson(data, {
       onEachFeature: function (feature, layer) {
         (function(layer, properties) {
-          // This creates numerical icons to match the ID numbers
-          // OR remove the next 6 lines for default blue Leaflet markers
           var numericMarker = L.ExtraMarkers.icon({
             icon: 'fa-number',
             number: feature.properties['id'],
@@ -86,7 +76,6 @@ function initMap() {
           });
           layer.setIcon(numericMarker);
 
-          // This creates the contents of each chapter from the GeoJSON data. Unwanted items can be removed, and new ones can be added
           var containerSource = $("#container-template").html();
           var containerTemplate = Handlebars.compile(containerSource);
 
@@ -102,28 +91,28 @@ function initMap() {
           var html = containerTemplate(output);
           $('#contents').append(html);
 
-          var i;
           var areaTop = -100;
           var areaBottom = 0;
 
-          // Calculating total height of blocks above active
-          for (i = 1; i < feature.properties['id']; i++) {
-            areaTop += $('div#container' + i).height() + imageContainerMargin;
-          }
-
-          areaBottom = areaTop + $('div#container' + feature.properties['id']).height();
+          // Recalculate areaTop and areaBottom based on the current order of elements
+          $('div#contents').children().each(function(index, element) {
+            if (element.id === 'container' + feature.properties['id']) {
+              areaBottom = areaTop + $(element).height();
+              return false; // break loop
+            }
+            areaTop += $(element).height() + imageContainerMargin;
+          });
 
           $('div#contents').scroll(function() {
             if ($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
               $('.image-container').removeClass("inFocus").addClass("outFocus");
               $('div#container' + feature.properties['id']).addClass("inFocus").removeClass("outFocus");
 
-              map.flyTo([feature.geometry.coordinates[1], feature.geometry.coordinates[0] ], feature.properties['zoom']);
+              map.flyTo([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], feature.properties['zoom']);
               TIMESLIDER.setDate(feature.properties['year']);
             }
           });
 
-          // Make markers clickable
           layer.on('click', function() {
             $("div#contents").animate({scrollTop: areaTop + "px"});
           });
@@ -138,5 +127,6 @@ function initMap() {
     geojson.addTo(map);
   });
 }
+
 
 initMap();
